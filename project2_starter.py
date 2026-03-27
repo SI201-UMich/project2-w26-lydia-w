e86c7ad01d5d0d19ef42a0a029c57972368474b1
wilkilyd <wilkilyd@umich.edu>
2026-03-27 15:15:58 -0400

    Name

--- project2_starter.py
+++ project2_starter.py
@@ -1,14 +1,7 @@
 # SI 201 HW4 (Library Checkout System)
-# Your name:
-# Your student id:
-# Your email:
-# Who or what you worked with on this homework (including generative AI like ChatGPT):
-# If you worked with generative AI also add a statement for how you used it.
-# e.g.:
-# Asked ChatGPT for hints on debugging and for suggestions on overall code structure
-#
-# Did your use of GenAI on this assignment align with your goals and guidelines in your Gen AI contract? If not, why?
-#
+# Your name: Lydia Wilkinson
+# Your student id: 8791224
+# Your email: wilkilyd
 # --- ARGUMENTS & EXPECTED RETURN VALUES PROVIDED --- #
 # --- SEE INSTRUCTIONS FOR FULL DETAILS ON METHOD IMPLEMENTATION --- #
 
